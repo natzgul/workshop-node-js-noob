19687cae0198d53be676de204c72d2191683258c
H.Nathan <nathan.hervouet@ynov.com>
2017-11-21 17:17:13 +0100

    Exercise 2, retourne un nbr en non un chiffre

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,14 @@
-exports.run = function(input) {
-  console.log("Exercise #2");
+exports.run = function (input) {
+    var chiffre = input.split(' ').map(Number);
+    var m = 0
+        , i = 0
+        , n = chiffre.length;
+    console.log(chiffre.length)
+    for (i = 0; i < n; i++) {
+        if (chiffre[i] > m) {
+            m = chiffre[i];
+            console.log(m)
+        }
+    }
+    return m
 };
